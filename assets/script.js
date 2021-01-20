5f3898dedfd86bdc2d8ba459274ea883e1425ebc
Gustavo <gustavo-dev@hotmail.com>
2021-01-20 12:00:43 -0300

    Adicionando numeros à tela

--- assets/script.js
+++ assets/script.js
@@ -8,11 +8,19 @@ let aviso = qS('.d-2')
 let lateral = qS('.d-1-right')
 
 let etapaAtual = 0
+let numero = ''
 
 function comecarEtapa(){
     let etapa = etapas[etapaAtual]
 
     let numeroHtml = ''
+
+    for(let i=0;i<etapa.numeros;i++){
+        if(i === 0){
+            numeroHtml += '<div class="numero pisca"></div>'
+        }
+        numeroHtml += '<div class="numero"></div>'
+    }
 
     seuVotoPara.style.display = 'none'
     cargo.innerHTML = etapa.titulo
@@ -21,9 +29,23 @@ function comecarEtapa(){
     lateral.innerHTML = ''
     numeros.innerHTML = numeroHtml
 }
+function atualizaInterface(){
+
+}
 
 function clicou(n) {
-    alert("clicou em "+n)
+    let elNumero = qS('.numero.pisca')
+    if(elNumero !== null){
+        elNumero.innerHTML=n
+        numero = `${numero}${n}`
+
+        elNumero.classList.remove('pisca')
+        if(elNumero.nextElementSibling !== null){
+            elNumero.nextElementSibling.classList.add('pisca')
+        } else {
+            atualizaInterface()
+        }
+    }
 }
 function branco() {
     alert("clicou em BRANCO")
