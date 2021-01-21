3840e3a95139d6ef147c7765af4ad4f1c96ef30b
Gustavo <gustavo-dev@hotmail.com>
2021-01-21 14:03:54 -0300

    Procurando Candidatos e informando, caso seja voto Nulo

--- assets/script.js
+++ assets/script.js
@@ -18,7 +18,7 @@ function comecarEtapa(){
     for(let i=0;i<etapa.numeros;i++){
         if(i === 0){
             numeroHtml += '<div class="numero pisca"></div>'
-        }
+        } else
         numeroHtml += '<div class="numero"></div>'
     }
 
@@ -30,6 +30,31 @@ function comecarEtapa(){
     numeros.innerHTML = numeroHtml
 }
 function atualizaInterface(){
+    let etapa = etapas[etapaAtual]
+    let candidato = etapa.candidatos.filter((item)=>{
+        if(item.numero === numero){
+            return true
+        } else {
+            return false
+        }
+    })
+    if(candidato.length > 0){
+        candidato = candidato[0]
+        seuVotoPara.style.display = "block"
+        aviso.style.display = "block"
+        descricao.innerHTML = `Nome: ${candidato.nome}<br>Partido: ${candidato.partido}`
+
+        let fotosHtml = ''
+        for(let i in candidato.fotos){
+            fotosHtml += `<div class="d-1-image"><img src="assets/images/${candidato.fotos[i].url}" alt="" srcset="">${candidato.fotos[i].legenda}</div>`
+        }
+
+        lateral.innerHTML = fotosHtml
+    } else{
+        seuVotoPara.style.display = 'block'
+        aviso.style.display = 'block'
+        descricao.innerHTML = '<div class="aviso--grande pisca">VOTO NULO</div>'
+    }
 
 }
 
@@ -47,6 +72,7 @@ function clicou(n) {
         }
     }
 }
+
 function branco() {
     alert("clicou em BRANCO")
 }
